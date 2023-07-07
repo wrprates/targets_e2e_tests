687ac5d4b51ee0247eebeb17081010fd0c0dda54
wrprates <wrprates@yahoo.com>
2023-07-07 16:04:09 -0300

    first working version, v1 of blog post also ready here

--- R/functions.R
+++ R/functions.R
@@ -1,45 +1,23 @@
 # R/functions.R
-library(purrr)
 library(shiny)
-library(targets)
 library(tibble)
 
 server <- function(input, output, session) { 
   output$plot <- renderPlot({
-    # browser()
     plot(input$param1, input$param2)
   })
 }
 
-# test_server_fn <- function(dataset) {
-#   print(dataset)
-#   shiny::testServer(server, {
-#     # browser()
-#     # Assuming that the datasets always have param1 and param2 columns
-#     session$setInputs(param1 = dataset$param1, param2 = dataset$param2)
-#     plot_created <<- !is.null(isolate(output$plot))
-#   })
-#   return(plot_created)
-# }
-
-test_server_fn <- function(data_name) {
-  # print(dataset)
+test_server_fn <- function(dataset) {
   shiny::testServer(server, {
-    # browser()
-    # Assuming that the datasets always have param1 and param2 columns
-    session$setInputs(
-      param1 = datasets$dataset[data_name][[1]]$param1,
-      param2 = datasets$dataset[data_name][[1]]$param2
-      
-    )
+    session$setInputs(param1 = dataset$param1, param2 = dataset$param2)
     plot_created <<- !is.null(isolate(output$plot))
   })
   return(plot_created)
 }
 
-# Define datasets as a list
+# Define datasets as a nested tibble
 create_demo_datasets <- function(dataset_list) {
-  # Create datasets tibble
   tibble(
     dataset_name = names(dataset_list),
     dataset = dataset_list
--- _targets.R
+++ _targets.R
@@ -1,67 +1,32 @@
 # _targets.R
 library(targets)
 library(tarchetypes)
-library(tibble)
 
 # Source functions
-source("R/functions.R")
+tar_source()
 
 # Define targets
-tar_option_set(packages = c("shiny", "purrr"), error = "continue")
+tar_option_set(packages = c("shiny", "tibble"), error = "continue")
 
 # Create the demo datasets
 datasets <- create_demo_datasets(
   dataset_list = list(
-    a = data.frame(param1 = c(1, 2), param2 = c(10, 12)),
-    b = data.frame(param1 = c(3, 4), param2 = c(13, 14))#,
-    # c = "wrong input"#,
-    # d = data.frame(param1 = c(5, 6), param2 = c(15, 16)),
-    # e = data.frame(param1 = c(7, 8, 9), param2 = c(17, 18, 19))
-    # ... you can add more datasets
+    a = quote(data.frame(param1 = c(1, 2), param2 = c(10, 12))),
+    b = quote(data.frame(param1 = c(3, 4), param2 = c(13, 14))),
+    c = quote(data.frame(param1 = c(5, 6), param2 = c(15, 16))),
+    d = "Wrong input.",
+    e = quote(data.frame(param1 = c(7:10), param2 = c(17:20)))
+    # ... you can add more datasets, try to add a string to force an error
+    # see https://github.com/ropensci/tarchetypes/discussions/105 
+    #. to understand the use of quote()
   )
 )
 
-data_names <- list("a" = "a", "b" = "b")
 # Static branching
 list(
   tar_map(
-    values = data_names,
-    names = "data_names",
-    tar_target(result, test_server_fn(data_names))
+    values = datasets,
+    names = "dataset_name",
+    tar_target(result, test_server_fn(dataset))
   )
 )
-
-# list(
-#   tar_map(
-#     values = datasets,
-#     names = "dataset_name",
-#     tar_target(result, test_server_fn(
-#       # datasets$dataset[[dataset_name]]
-#       dataset[[dataset_name]]
-#     ))
-#   )
-# )
-
-# list(
-#   tar_map(
-#     values = datasets,
-#     names = "dataset_name",
-#     tar_target(result, test_server_fn(dataset))
-#   )
-# )
-# 
-# datasets <- tibble::tibble(
-#   name = c("a", "b"),
-#   data = list(
-#     a = list(param1 = c(1, 2), param2 = c(10, 12)), 
-#     b = list(param1 = c(3, 4), param2 = c(13, 14))
-#   )
-# )
-
-# list(
-#   tar_map(
-#     values = names,
-#     # names = "name",
-#     tar_target(result, test_server_fn(data[[names]]))
-#   )
-# )
